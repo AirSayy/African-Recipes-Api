cc9250b40002ea873afa33eaae09628620eb8711
Eseoghene Eguakhide <eruotorese@yahoo.com>
2023-02-02 18:46:34 +0100

    api creation

--- food-recipe-api/server.js
+++ food-recipe-api/server.js
@@ -4,26 +4,96 @@ const app = express()
 const PORT = 7000
 
 const meals = {
-    'akara' : {
-        'type of meal' : 'Breakfast',
-        'origin': 'Nigeria',
-    },
+    'rice' : {
+        ['fried rice'] : {
+           [ 'ingredients 1' ]: 
+                [
+                    'chicken 1.2kg',
+                    'thumb sized ginger (chopped or minced',
+                    '3 cloves garlic (chopped)',
+                    '1 small medium onions',
+                    '1 teaspoon salt',
+                    '1 1/2 teaspoon curry powder',
+                    '3 stem fresh thyme / 1 teaspoon dried thyme'
+                ],
+            ['ingredients 2'] :
+                [
+                    '3 cups of rice (basmatti rice)',
+                    '2 cups meat stock',
+                    '1 cup water',
+                    '2 teaspoon curry powder',
+                    '1 teaspoon white pepper',
+                    '1/2 green bell pepper'
+                ],
+            ['ingredients 3'] : 
+                [
+                    '1 bunch spring onion',
+                    '1 carrot',
+                    '1 bell pepper',
+                    'a cup of sweet corn',
+                    'a cup of green beans',
+                    'half teaspoon salt, garlic powder, curry, thyme',
+                    '2 teaspoon white pepper',
+                    '1 chicken cube',
+                    'diced sausuages'
+                ],
+            'instructions' :
+                [
+                    '1 : in a pot lorem ispum ...'
+                ]
+                
+                
+                    
+                
+                
+            
+                
+            },
+        ['jollof rice'] : {
+                ['ingredients 1'] :
+                [
+                    '8 cups of rice',
+                    '3 medium bell pepper',
+                    '5 pieces of tomato',
+                    '2 scotch bonnet pepper',
+                    'half onion',
+                    '3 cloves of galic',
+                    '2 ginger'
+                ]
+            },
+        ['coconut rice']:{
+            ['ingredients'] :
+            [
 
-    'yam' : {
-        'type of meal' : 'Lunch',
-        'preparation-type': 'Boiled',
-    },
+            ],
+            ['instructions']:
+            [
 
-    'coconut-rice' : {
-        'type of meal' : 'Lunch',
-        'origin': 'Nigeria',
-    },
+            ]
+        },
+        ['native rice']:{
+            ['ingredients']:
+            [
 
-    'pap' : {
-        'type of meal' : 'oops',
-        'origin': 'oops',
+            ],
+            ['instructions']:
+            [
+
+            ]
+        }
+        },
+        'yam':{
+            ['yam peppersoup'] :{
+                ['ingredients']:
+                [
+                    
+                ]
+            }
+        }
     }
-}
+
+    
+
 
 app.get('/' , (request , response) => {
     response.sendFile(__dirname + '/index.html')
@@ -40,7 +110,7 @@ app.get('/api/:mealName' , (req , res) => {
 
 
 
-    // res.json(meals)
+    //  res.json(meals)
 })
 
 app.listen(PORT , () => {
